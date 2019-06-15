9430e32a7da29647bc53a0c73901adf378e765a4
Jason003 <792576519@qq.com>
2019-06-15 21:33:36 +0800

    initialize socket.io

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,39 @@
-io();
+const socket = io();
+
+socket.on('welcome', message => {
+  console.log(message);
+});
+
+socket.on('message', message => {
+  console.log(message);
+});
+
+socket.on('location', ({ longitude, latitude }) => {
+  console.log(longitude);
+  console.log(latitude);
+});
+
+$('#message-form').submit(e => {
+  e.preventDefault();
+  let message = e.target.elements.message.value;
+  message = $.trim(message);
+  if (message === '') {
+    alert('Please enter something!');
+    return;
+  }
+  $('#messageTextarea').val('');
+  socket.emit('message', message, error => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log('Delivered Successfully!');
+  });
+});
+
+$('#sendLocationButton').click(e => {
+  e.preventDefault();
+  navigator.geolocation.getCurrentPosition(location => {
+    const { longitude, latitude } = location.coords;
+    socket.emit('location', { longitude, latitude });
+  });
+});
--- src/index.js
+++ src/index.js
@@ -2,18 +2,35 @@ const path = require('path');
 const express = require('express');
 const http = require('http');
 const socketio = require('socket.io');
+const Filter = require('bad-words');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+const filter = new Filter();
 
 const port = process.env.PORT || 3001;
 const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
-io.on('connection', () => {
+io.on('connection', socket => {
   console.log('New WebSocket Connection...');
+  socket.emit('welcome', 'Welcome!');
+  socket.broadcast.emit('welcome', 'A new user entered');
+  socket.on('message', (message, callback) => {
+    if (filter.isProfane(message)) {
+      return callback('Profane if not allowed!');
+    }
+    io.emit('message', message);
+    callback();
+  });
+  socket.on('disconnect', () => {
+    io.emit('welcome', 'A user left');
+  });
+  socket.on('location', ({ longitude, latitude }) => {
+    io.emit('location', { longitude, latitude });
+  });
 });
 
 server.listen(port, () => {
